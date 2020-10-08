e2e49362113bfff2edc3e982f3d0537d49ab1ea8
Lucas Souza <souzacontato18@gmail.com>
2020-10-08 17:23:42 -0300

    Console adicionado

--- VisualGraphs/Classes/TextConsole.cs
+++ VisualGraphs/Classes/TextConsole.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
 namespace VisualGraphs.Classes
 {
     class TextConsole
     {
         private Queue<string> ConsoleQueue;
+        private TextBox Box;
 
-        public TextConsole()
+        public TextConsole(TextBox t)
         {
+            Box = t;
+            ConsoleQueue = new Queue<string>();
+        }
 
+        public void AddStringToConsole(string s)
+        {
+            if(ConsoleQueue.Count() == 5)
+            {
+                ConsoleQueue.Dequeue();
+                ConsoleQueue.Enqueue(s);
+            }
+            ConsoleQueue.Enqueue(s);
+        }
+
+        public void SetTextBox(string str)
+        {
+            Box.Text = str;
         }
 
 
