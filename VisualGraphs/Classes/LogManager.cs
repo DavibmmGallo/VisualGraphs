04adc80137aacc7ac7fa2380e159805cb43019e9
Lucas Souza <souzacontato18@gmail.com>
2020-10-19 01:05:07 -0300

    Inicio dos Logs

---
+++ VisualGraphs/Classes/LogManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace VisualGraphs.Classes
+{
+    class LogManager
+    {
+        private string pathDirectory;
+
+        public LogManager(string path)
+        {
+            pathDirectory = path;
+            if(!Directory.Exists(pathDirectory))
+            {
+                DirectoryInfo info = Directory.CreateDirectory(pathDirectory);
+                Debug.WriteLine("Diretorio criado em : {0}", pathDirectory);
+            }
+        }
+    }
+}
